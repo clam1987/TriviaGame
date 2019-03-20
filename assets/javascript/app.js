f72f77448da504a363e9beb44fac61706a410eeb
Cuong Lam <lin-1987@live.com>
2019-03-19 21:40:49 -0700

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,85 @@
+//Variables
+var questions = [
+    {q: "What is Monica's biggest pet peeve?", 
+     c: ["Leaving Messes", "Losing to Chandler", "Animals dressed as Humans", "Ross getting his way"], 
+     a:2
+    },
+    {q: "According to Chandler, what phenomenon scares the bejeezus out of him?",
+     c: ["Michael Flatley, Lord of the Dance!"],
+     a:3
+    },
+    {q: "Monica and Ross had a grandmother who died. Chandler and Joey went to her funeral. Name that grandmother",
+     c: ["Althea"],
+     a:1
+    },
+    {q: "Every week, the TV Guide comes to Chandler and Joey's apartment. What name appears on the address label?",
+     c: ["Miss Chanandler Bong"],
+     a:1
+    },
+    {q: "What is the name of Chandler's father's Las Vegas all-male burlesque show?",
+     c: ["Viva Las Gaygas!"],
+     a:1
+    },
+    {q: "What was Monica's nickname when she was a field hockey goalie?",
+     c: ["Big Fat Goalie"],
+     a:1
+    },
+    {q: "Rachel claims this is her favorite movie...",
+     c: ["Dangerous Liaisons"],
+     a:1
+    },
+    {q: "Her actual Favorite movie is...",
+     c: ["Weekend at Bernies"],
+     a:1
+    },
+    {q: "In what part of her body did Monica get a pencil stuck at age 14?",
+     c: ["Her Ear"],
+     a:1
+    },
+    {q: "Monica categorizes her towls. How many categories are there?",
+     c: ["11"],
+     a:1
+    },
+    {q: "What is Joey's favorite food?",
+     c: ["sandwiches"],
+     a:1
+    },
+    {q: "Chandler was hold old when he first touched a girl's breast?",
+     c: ["19"],
+     a:1
+    },
+    {q: "Joey had an imaginary childhood friend. What was his name?",
+     c: ["Maurice"],
+     a:1
+    },
+    {q: "And his Profession was?",
+     c: ["Space Cowboy"],
+     a:1
+    },
+    {q: "What is Chandler Bing's Job?",
+     c: ["Statistical Analysis and Data Reconfiguration"],
+     a:1
+    },
+]
+
+
+
+
+
+
+//Functions
+
+
+
+
+
+
+
+//Console logs
+console.log(questions);
+
+
+
+
+
+//Processes
