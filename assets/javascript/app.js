c052675df83bcf381c4887f573ff6ce260d4069b
Cuong Lam <lin-1987@live.com>
2019-03-21 22:44:16 -0700

    1.0 beta

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,61 +7,62 @@ var questions = [
     },
     {q: "According to Chandler, what phenomenon scares the bejeezus out of him?",
      c: ["Michael Flatley, Lord of the Dance!", "The Trappings of Intimacy", "The Pool Boy who Broke his Parents up", "Richard Burke- Monica's ex"],
-     a:1
+     a:0
     },
     {q: "Monica and Ross had a grandmother who died. Chandler and Joey went to her funeral. Name that grandmother",
      c: ["Nana","Judy","Lily","Althea"],
-     a:4
+     a:3
     },
     {q: "Every week, the TV Guide comes to Chandler and Joey's apartment. What name appears on the address label?",
      c: ["Miss Chancy Bang", "Miss Chandler Bing", "Miss Chaneel Bong","Miss Chanandler Bong"],
-     a:4
+     a:3
     },
     {q: "What is the name of Chandler's father's Las Vegas all-male burlesque show?",
      c: ["It's Raining Men", "Viva Las Gaygas!", "Chipindeals", "Magic Mike Express"],
-     a:2
+     a:1
     },
     {q: "What was Monica's nickname when she was a field hockey goalie?",
      c: ["The Big Gate","The Wall","Big Fat Goalie","The Bigger Goalie"],
-     a:3
+     a:2
     },
     {q: "Rachel claims this is her favorite movie...",
      c: ["Dangerous Liaisons", "Taxi Driver", "Godfather", "Unbreakable"],
-     a:1
+     a:0
     },
     {q: "Her actual Favorite movie is...",
      c: ["Space Jam","Ghost Busters","Police Academy","Weekend at Bernies"],
-     a:4
+     a:3
     },
     {q: "In what part of her body did Monica get a pencil stuck at age 14?",
      c: ["Her Mouth", "Her Nose", "Her Ear", "Her Hand"],
-     a:3
+     a:2
     },
     {q: "Monica categorizes her towls. How many categories are there?",
      c: ["5", "11", "8", "16"],
-     a:2
+     a:1
     },
     {q: "What is Joey's favorite food?",
      c: ["pizza", "cake", "turkey","sandwiches"],
-     a:4
+     a:3
     },
     {q: "Chandler was hold old when he first touched a girl's breast?",
      c: ["21","16","25","19"],
-     a:4
+     a:3
     },
     {q: "Joey had an imaginary childhood friend. What was his name?",
      c: ["John", "Maurice", "Kenny", "Gunther"],
-     a:2
+     a:1
     },
     {q: "And his Profession was?",
      c: ["Superhero", "Doctor", "Space Cowboy", "Firefighter"],
-     a:3
+     a:2
     },
     {q: "What is Chandler Bing's Job?",
      c: ["Transponster", "Statistical Analysis and Data Reconfiguration", "Technical Analyst", "Stock Broker"],
-     a:2
-    }
-]
+     a:1
+    
+    }];
+
 
 
 
@@ -69,7 +70,7 @@ var questions = [
 var winCounter = 0;
 var loseCounter = 0; 
 var timer = 10;
-var unansweredCounter = 0;
+var unanswerCounter = 0;
 var intervalId;
 var userGuess = "";
 var running = false;
@@ -80,7 +81,7 @@ var newArray = [];
 var holder = [];
 
 
-
+$("#reset").hide();
 //Functions
 // Used to start the game
 $("#start").on("click", function () {
@@ -106,11 +107,14 @@ function decrement() {
 
         //what happens if timer is at 0
         if (timer === 0) {
-            unansweredCounter++;
+            unanswerCounter++;
             stop();
             $("#answer-choice").html("<p>You've been bamboozled! The correct answer is " + pick.c[pick.a] + "</p>");
+            nextquestion();
         }
 }
+
+
 // Function to stop Timer
 function stop() {
     running = false;
@@ -123,7 +127,7 @@ function displayQuestion() {
     index = Math.floor(Math.random() * questions.length);
     pick = questions[index];
 
-    $("#questions").html("<h2>" + pick.questions + "</h2>");
+    $("#questionsOutput").html("<h2>" + pick.q + "</h2>");
     for (var i = 0; i < pick.c.length; i++) {
         var userChoice = $("<div>");
         userChoice.addClass("answerchoice");
@@ -133,52 +137,89 @@ function displayQuestion() {
         $("#answer-choice").append(userChoice);
     }
 
-}
-
-//Click Function for answer selection
-$(".answerchoice").on("click", function(){
+    //Click Function for answer selection
+$(".answerchoice").on("click", function() {
     // grabs array from the user
     userGuess = parseInt($(this).attr("data-guessvalue"));
-
+  
     //correct guesses vs non correct guesses
     if (userGuess === pick.a) {
         stop();
         winCounter++;
         userGuess = "";
         $("#answer-choice").html("<p>How You Doin? You got it right!</p>");
+        nextquestion();
+    
     } else {
         stop();
         loseCounter++;
+        userGuess="";
+        $("#answer-choice").html("<p>You've been bamboozled! The correct answer is " + pick.c[pick.a] + "</p>");
+        nextquestion();
+
     }
 });
 
-// Reset Button Function
-$("#reset").on("click", function(){
-    $("#answer-choice").empty();
-    $("#questions").empty();
-    $("#reset").hide();
-    for (var i = 0; i < holder.length; i++) {
-        Option.push(holder[i]);
+}
+
+
+
+//Function to display next question + score at the end
+function nextquestion() {
+    $("#answer-choice").append();
+    newArray.push(pick);
+    questions.splice(index,1);
+
+    var timeout = setTimeout(function() {
+        $("#answerblock").empty();
+        timer = 10;
+
+    //run score screen once all questions finish
+    if ((winCounter + loseCounter + unanswerCounter) === qCount) {
+        $("#questionsOutput").empty();
+        $("#questionsOutput").html("<h1>Game Over! Your results : </h1>");
+        $("#answer-choice").append("<h5> Correct: "+ winCounter + "</h5>");
+        $("#answer-choice").append("<h5> Incorrect: " + loseCounter + "</h5>");
+        $("#answer-choice").append("<h5> Unanswered: " + unanswerCounter + "</h5>");
+        $("#reset").show();
+        winCounter = 0;
+        loseCounter = 0;
+        unanswerCounter = 0;
+
+    } else {
+        startTimer();
+        displayQuestion();
     }
 
+
+
+
+
+
+
+    }, 1000);
+}
+
+
+
+
+
+
+
+// Reset Button Function
+$("#reset").on("click", function() {
+    $("#reset").hide();
+    $("#answer-choice").empty();
+    $("#questionsOutput").empty();
+    for (var i = 0; i < holder.length; i++) {
+        questions.push(holder[i]);
+    }
     startTimer();
     displayQuestion();
-
-
-
-
-
 })
 
 
-
-
-
-
-
-
-
-});
+})
 
 
 
